cb6b9d93df99c806a170c6f58e3a5677ec759d95
Marceline-G-S <Marceline.Silversmith@gmail.com>
2024-05-30 11:15:46 -0500

    Added crud functionality to json-server and user view.

--- views/user.py
+++ views/user.py
@@ -68,3 +68,107 @@ def create_user(user):
         id = db_cursor.lastrowid
 
         return json.dumps({"token": id, "valid": True})
+
+
+def update_user(id, Users_data):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            UPDATE Users
+            SET first_name =?,
+            last_name =?,
+            email =?,
+            bio =?,
+            username =?,
+            password =?,
+            profile_image_url =?,
+            created_on =?,
+            active =?
+            WHERE id =?
+            """,
+            (   Users_data["first_name"],
+                Users_data["last_name"],
+                Users_data["email"],
+                Users_data["bio"],
+                Users_data["username"],
+                Users_data["password"],
+                Users_data["profile_image_url"], 
+                Users_data["created_on"],
+                Users_data["active"],
+                id),
+        )
+
+        rows_affected = db_cursor.rowcount
+
+    return True if rows_affected > 0 else False
+
+
+def delete_user(pk):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute(
+            """
+        DELETE FROM Users WHERE id = ?
+        """,
+            (pk,),
+        )
+        number_of_rows_deleted = db_cursor.rowcount
+
+    return True if number_of_rows_deleted > 0 else False
+
+
+def list_users():
+    # Open a connection to the database
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute(
+            """
+        SELECT
+            *
+        FROM Users s
+        """
+        )
+        query_results = db_cursor.fetchall()
+
+        # Initialize an empty list and then add each dictionary to it
+        Users = []
+        for row in query_results:
+            Users.append(dict(row))
+
+        # Serialize Python list to JSON encoded string
+        serialized_Users = json.dumps(Users)
+
+    return serialized_Users
+
+
+def retrieve_user(pk):
+    # Open a connection to the database
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute(
+            """
+        SELECT
+            *
+        FROM Users s
+        WHERE s.id = ?
+        """,
+            (pk,),
+        )
+        query_results = db_cursor.fetchone()
+
+        # Serialize Python list to JSON encoded string
+        dictionary_version_of_object = dict(query_results)
+        serialized_Users = json.dumps(dictionary_version_of_object)
+
+    return serialized_Users
